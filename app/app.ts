8447d2f19c5dba05a6443b2ca807caae53f94171
Hiroiku Inoue <work@hiroiku.me>
2024-01-16 14:08:10 +0900

    修正(README.md): 使用方法の説明を改善し、テストケースのディレクトリを指定する新機能を追加
    修正(app.ts): テストケースディレクトリのオプションを追加
    この変更により、ユーザーはテストケースのディレクトリを指定できるようになります。
    
    削除(.gitkeep): テストディレクトリの.gitkeepファイルを削除
    テストディレクトリにファイルが追加されたため、.gitkeepファイルは不要になりました。

--- app/app.ts
+++ app/app.ts
@@ -14,75 +14,89 @@ const manager = new ProcessManager({
   '.js': NodeProcess,
 });
 
-program.argument('<file>', 'Execution file').action(async file => {
-  // テストケースを ./tests/*.txt から読み込む
-  const projectDirectory = path.resolve(__dirname, '..');
-  const testCasePaths = glob.sync(path.resolve(projectDirectory, 'tests', '**/*.txt')).sort();
+program
+  .argument('<file>', 'Execution file')
+  .option('-t, --test <dir>', 'Test cases directory')
+  .action(async (file, options) => {
+    // プロジェクトのルートディレクトリを取得する
+    const projectDirectory = path.resolve(__dirname, '..');
 
-  for (const testCasePath of testCasePaths) {
     // 実行ファイルのパスを取得する
-    const executionFile = path.resolve(projectDirectory, file);
+    const executionFilePath = path.resolve(file);
 
-    // 実行ファイルが存在しなければエラーを投げる
-    if (!fs.existsSync(executionFile)) {
-      console.error(colors.bgRed(' ERROR '), colors.bold('Execution file is not found.'));
-      console.error(colors.red(executionFile));
+    // テストケースを ./tests/*.txt から読み込む
+    const testCaseDirectory = (() => {
+      if (options.test) {
+        return path.resolve(options.test);
+      }
 
-      return;
-    }
+      // options.test がなければ実行ファイルと同じディレクトリの ./tests/ を対象とする
+      return path.resolve(path.dirname(executionFilePath), 'tests');
+    })();
 
-    // ファイルの拡張子を取得する
-    const extension = path.extname(executionFile);
+    const testCasePaths = glob.sync(path.resolve(testCaseDirectory, '**/*.txt')).sort();
 
-    // 拡張子が対応していなければエラーを投げる
-    if (!manager.isProcessKey(extension)) {
-      console.error(colors.bgRed(' ERROR '), colors.bold(`Invalid file extension. (${extension})`));
-      console.error('Please see README.md.');
+    for (const testCasePath of testCasePaths) {
+      // 実行ファイルが存在しなければエラーを投げる
+      if (!fs.existsSync(executionFilePath)) {
+        console.error(colors.bgRed(' ERROR '), colors.bold('Execution file is not found.'));
+        console.error(colors.red(executionFilePath));
 
-      return;
-    }
+        return;
+      }
 
-    // テストケースファイルを読み込む
-    const testCase = fs.readFileSync(testCasePath, 'utf-8');
-    const [input, output] = testCase
-      .trim()
-      .split(/入力例.*?\n|出力例.*?\n/)
-      .map(text => text.trim())
-      .slice(1);
+      // ファイルの拡張子を取得する
+      const extension = path.extname(executionFilePath);
 
-    // 拡張子に応じたプロセスを実行する
-    const processConstructor = manager.get(extension);
-    const process = new processConstructor(executionFile);
-    const result = await process.execute(input);
-    const stdout = result.stdout.trim();
+      // 拡張子が対応していなければエラーを投げる
+      if (!manager.isProcessKey(extension)) {
+        console.error(colors.bgRed(' ERROR '), colors.bold(`Invalid file extension. (${extension})`));
+        console.error('Please see README.md.');
 
-    // テストケースの判定結果を表示する
-    const testCaseName = path.relative(projectDirectory, testCasePath);
+        return;
+      }
 
-    if (stdout === output) {
-      console.info(colors.bgGreen(' PASS '), colors.bold(testCaseName));
-    } else {
-      console.error(colors.bgRed(' FAIL '), colors.bold(testCaseName));
-    }
-    console.log('');
+      // テストケースファイルを読み込む
+      const testCase = fs.readFileSync(testCasePath, 'utf-8');
+      const [input, output] = testCase
+        .trim()
+        .split(/入力例.*?\n|出力例.*?\n/)
+        .map(text => text.trim())
+        .slice(1);
 
-    // メモリ使用量と実行時間を表示する
-    const [size, unit] = bytes(result.memoryUsage, { unitSeparator: ' ' }).split(' ');
-    console.info(colors.blue('Memory Usage:'), colors.yellow(size), unit);
-    console.info(colors.blue('Elapsed Time:'), colors.yellow(result.elapsedTime.toString()), 'sec');
-    console.log('');
+      // 拡張子に応じたプロセスを実行する
+      const processConstructor = manager.get(extension);
+      const process = new processConstructor(executionFilePath);
+      const result = await process.execute(input);
+      const stdout = result.stdout.trim();
 
-    // 標準出力を表示する
-    if (stdout) {
-      console.log(stdout);
+      // テストケースの判定結果を表示する
+      const testCaseName = path.relative(projectDirectory, testCasePath);
+
+      if (stdout === output) {
+        console.info(colors.bgGreen(' PASS '), colors.bold(testCaseName));
+      } else {
+        console.error(colors.bgRed(' FAIL '), colors.bold(testCaseName));
+      }
       console.log('');
-    }
 
-    // エラーがあれば表示する
-    if (result.stderr) {
-      console.log(colors.red(result.stderr));
+      // メモリ使用量と実行時間を表示する
+      const [size, unit] = bytes(result.memoryUsage, { unitSeparator: ' ' }).split(' ');
+      console.info(colors.blue('Memory Usage:'), colors.yellow(size), unit);
+      console.info(colors.blue('Elapsed Time:'), colors.yellow(result.elapsedTime.toString()), 'sec');
+      console.log('');
+
+      // 標準出力を表示する
+      if (stdout) {
+        console.log(stdout);
+        console.log('');
+      }
+
+      // エラーがあれば表示する
+      if (result.stderr) {
+        console.log(colors.red(result.stderr));
+      }
     }
-  }
-});
+  });
 
 program.parse(process.argv);
